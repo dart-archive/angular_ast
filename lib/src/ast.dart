3c0ace3a108da3195764d77e64ca3301365ff980
Matan Lurey <matanl@matanl-macbookpro2.roam.corp.google.com>
2016-12-13 17:17:07 -0800

    Add missing license headers

--- lib/src/ast.dart
+++ lib/src/ast.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 export 'package:angular_ast/src/ast/attribute.dart' show AttributeAst;
 export 'package:angular_ast/src/ast/comment.dart' show CommentAst;
 export 'package:angular_ast/src/ast/content.dart' show EmbeddedContentAst;
--- lib/src/ast/content.dart
+++ lib/src/ast/content.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 import 'package:angular_ast/src/ast.dart';
 import 'package:angular_ast/src/token.dart';
 import 'package:source_span/source_span.dart';
