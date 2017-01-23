66409b9e32bbf9912dce9333bc9e11c20ae47b62
Max Kim <maxkim@google.com>
2017-01-23 10:01:24 -0800

    Copyright fixes and removing unnecessary comment block

--- test/simple_tokenizer_test.dart
+++ test/simple_tokenizer_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -12,16 +12,6 @@ void main() {
   String untokenize(Iterable<NgSimpleToken> tokens) => tokens
       .fold(new StringBuffer(), (buffer, token) => buffer..write(token.lexeme))
       .toString();
-
-  /**
-  test('', () {
-    expect(
-      tokenize(''),
-      [
-      ]
-    );
-  });
-  **/
 
   test('should tokenize plain text', () {
     expect(tokenize('Hello World'), [
